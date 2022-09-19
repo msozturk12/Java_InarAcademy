f0876461b2bf63101daf05aa738ffac179f63da0
Mesut Said OZTURK <msozturk12@gmail.com>
2022-09-19 14:40:51 +0200

    reviewed

--- src/chapters/chapter05/chapter05/Exercises_05_12.java
+++ src/chapters/chapter05/chapter05/Exercises_05_12.java
@@ -5,18 +5,23 @@ import java.util.Scanner;
 public class Exercises_05_12 {
 
 	public static void main(String[] args) {
-				
+
+		/**
+		 * Find the smallest n such that n^2 > 12,000)
+		 * n^2 > 24
+		 * n^2<24 ---> en büyük n=4  n dersem bulurum
+		 */
+
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter a number ");
+		System.out.println("Enter a number");
 		int number = input.nextInt();
-		int n = 1;
-		
-		while(Math.pow(n, 2) < number) {            //n^2>number the smallest n
+
+		int n =0;
+		while(Math.pow(n,2)<number){
+
 			n++;
-			
 		}
-	System.out.println("The smallest integer n is " + n);
-	
+		System.out.println("The smallest integer n such that n^2 is greater than " + number + " is:" + " n= " +n);
 	}
 
 }
