5d48cdb9d7d350f95497fd195e814bbb059050b5
Mesut Said OZTURK <msozturk12@gmail.com>
2022-10-26 14:23:56 +0300

    reviewed-8.15

--- src/chapters/chapter08/Exercises08/Exercises08_15.java
+++ src/chapters/chapter08/Exercises08/Exercises08_15.java
@@ -5,23 +5,19 @@ import java.util.Scanner;
 public class Exercises08_15 {
     public static void main(String[] args) {
         /*
-         * 1 - Anlat
-         * 2 - Ornekle
-         * 3 - Algoritmani sozel
-         * 4 - Test case olustur
+        Geometry: same line?)
+         * 1 - Anlat  we take 5 point and figure out is all tahat point on the same line
+         * 2 - Ornekle ex. 1,1 2,2 3,3 4,4 5,5 are on the same line
+         * 3 - Algoritmani sozel  (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0) == 0 same line formülü
+         * 4 - Test case olustur ex. 1,1 2,2 3,3 4,4 -5,2 are not on the same line
          * 5 - Kod yaz
-         * 6 - test et
+         * 6 - test et11
 
          */
-        Scanner input = new Scanner(System.in);
 
         double[][] points = getPointsFromUsers();
-        if (sameLine(points)) {
-            System.out.println("The five points are on the same line");
-        } else {
-            System.out.println("The five points are not on the same line");
-        }
 
+        System.out.println("The five points are " + (sameLine(points) ? "" : "not") + " on the same line");
 
     }
 
@@ -38,26 +34,18 @@ public class Exercises08_15 {
     }
 
     public static boolean sameLine(double[][] points) {
-        double x0 = points[0][0];
-        double y0 = points[0][1];
-        double x1 = points[1][0];
-        double y1 = points[1][1];
+        for (int i = 0; i < points.length; i++) {
+            if (pointPosition(points[i][0], points[i][1], points[i + 1][0], points[i + 1][1], points[i + 2][0], points[i + 2][1]) != 0) {
+                return false;
+            }
+        }
 
-        double x2 = points[2][0];
-        double y2 = points[2][1];
-
-        double x3 = points[3][0];
-        double y3 = points[3][1];
-        double x4 = points[4][0];
-        double y4 = points[4][1];
-        //(x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0) == 0;
-
-        double m = (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);//ilk üç nokta aynı line da mı
-        double n = (x3 - x2) * (y4 - y2) - (x4 - x2) * (y3 - y2);//son üç nokta aynı line da mı
-
-        return (m == 0 && n == 0);
-
+        return true;
     }
 
+    // (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0) == 0; -->is same line, we re checking this
+    public static double pointPosition(double x0, double y0, double x1, double y1, double x2, double y2) {
+        return ((x1 - x0) * (y2 - y0)) - ((x2 - x0) * (y1 - y0));
 
+    }
 }
